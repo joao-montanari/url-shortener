6cae5abdcc2546a84fa9d996090efd79b5c67a8c
João Vitor Montanari <joao.unipbr@gmail.com>
2025-05-11 21:54:22 -0300

    :recycle: refactor: Adiciona o componente Loading a página de redirecionamento

--- src/Pages/Redirect/index.tsx
+++ src/Pages/Redirect/index.tsx
@@ -1,9 +1,12 @@
+
+import LoadingComponent from '../../components/Loading';
+
 import './style.css';
 
 const RedeirectPage = () => {
     return (
         <div className='redirect-page-master-container'>
-            
+            <LoadingComponent/>
         </div>
     )
 }
