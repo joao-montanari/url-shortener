e86d9c2ad8b4a827f9f3dd9f75d8758ca97f2fd1
João Vitor Montanari <joao.unipbr@gmail.com>
2025-05-11 23:04:01 -0300

    :sparkles: feat: Requisição dos links cadastrados

--- src/Pages/Redirect/index.tsx
+++ src/Pages/Redirect/index.tsx
@@ -1,9 +1,25 @@
 
+import { useEffect } from 'react';
+import { collection, addDoc, getDocs, Timestamp } from "firebase/firestore";
+
 import LoadingComponent from '../../components/Loading';
+import { db } from '../../utils/firebase-config';
 
 import './style.css';
 
 const RedeirectPage = () => {
+
+    async function getLinks() {
+        const querySnapshot = await getDocs(collection(db, "urls"));
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} =>`, doc.data());
+        });
+    }
+
+    useEffect(() => {
+        getLinks();
+    }, []);
+
     return (
         <div className='redirect-page-master-container'>
             <LoadingComponent/>
