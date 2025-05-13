8b44f1ba29f454082ee3c686ba42a24b4355d5b7
joao-montanari <joao.unipbr@gmail.com>
2025-05-13 08:40:58 -0300

    :sparkles: feat(translate): Configura o i18n e adiciona as primeiras referencias de tradução

--- i18n.ts
+++ i18n.ts
@@ -1,22 +1,22 @@
-// import i18n from 'i18next';
-// import { initReactI18next } from 'react-i18next';
-// import HttpApi from 'i18next-http-backend';
-// import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-// i18n
-//     .use(HttpApi)
-//     .use(LanguageDetector)
-//     .use(initReactI18next)
-//     .init({
-//         fallbackLng: 'pt',
-//         debug: true,
-//         interpolation: {
-//             escapeValue: false,
-//         },
-//         backend: {
-//             loadPath: '/i18n/{{lng}}.json'
-//         }
-//     });
+i18n
+    .use(HttpApi)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'pt',
+        debug: true,
+        interpolation: {
+            escapeValue: false,
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}.json'
+        }
+    });
 
 
-// export default i18n;
+export default i18n;
