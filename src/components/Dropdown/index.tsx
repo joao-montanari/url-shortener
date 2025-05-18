caffaf68ffa1336dfe3e2a4114457ee59a5b689c
João Vitor Montanari <joao.unipbr@gmail.com>
2025-05-18 15:04:20 -0300

    :construction: feat: Continuação no desenvolvimento do componente Dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,33 +1,58 @@
 import { useState } from 'react';
+import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
+import { SvgIcon } from '@mui/material';
+
 import './style.css';
 
 export interface DropdownProps {
     items: string[];
     setValue: (item: string) => void;
     value: string;
+    styleButton?: React.CSSProperties;
+    styleOptionsBox?: React.CSSProperties;
+    styleOption?: React.CSSProperties;
 }
 
 const Dropdown = ({
     items,
     setValue,
     value,
+    styleButton,
+    styleOptionsBox,
+    styleOption,
 }: DropdownProps) => {
     const [showOptions, setShowOptions] = useState<boolean>(false);
 
+    const onSelectOption = (item: string) => {
+        setValue(item);
+        setShowOptions(false);
+    }
+
     return (
         <div className="dropdown-container-global">
-            <button onClick={() => setShowOptions(!showOptions)}>
-                {value}
+            <button 
+                style={styleButton}
+                onClick={() => setShowOptions(!showOptions)}
+            >
+                <span>{value}</span>
+                <SvgIcon 
+                    component={ showOptions ? KeyboardArrowUp : KeyboardArrowDown} 
+                    className='button-component-icon'
+                />
             </button>
             {
                 showOptions && (
-                    <div className="dropdown-options-area">
+                    <div 
+                        className="dropdown-options-area"
+                        style={styleOptionsBox}
+                    >
                         {
                             items.map((item, i) => (
                                 <div 
                                     key={`option-item-${i}`} 
                                     className="option-content"
-                                    onClick={() => setValue(item)}
+                                    onClick={() => onSelectOption(item)}
+                                    style={styleOption}
                                 >
                                     {item}
                                 </div>
